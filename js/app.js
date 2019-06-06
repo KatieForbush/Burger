1da4b51b420c646556cf64db9e2c76bda734df86
Kate <you@example.com>
2019-06-05 20:44:09 -0700

    adding the rest of my app.js and my ORM

--- js/app.js
+++ js/app.js
@@ -24,8 +24,34 @@ $(function(){
         event.preventDefault();
 
         var newBurger = {
-            name: 
-        }
+            name: $("#burg").val().trim(),
+            favorite: $("[burg=favorite]:checked").val().trim()
 
-    })
-})
+        };
+        $.ajax("/api/cats", {
+            type: "POST",
+            data: newFav
+          }).then(
+            function() {
+              console.log("Made another Fav");
+              // Reload the page to get the updated list
+              location.reload();
+            }
+          );
+        });
+        $(".delete-burger").on("click", function(event) {
+            var id = $(this).data("id");
+        
+            // Send the DELETE request.
+            $.ajax("/api/burgers/" + id, {
+              type: "DELETE"
+            }).then(
+              function() {
+                console.log("deleted burger", id);
+                // Reload the page to get the updated list
+                location.reload();
+              }
+            );
+          });
+
+    });
